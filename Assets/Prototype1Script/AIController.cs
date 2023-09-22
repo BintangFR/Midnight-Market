cb98f3f602de04c768a17368daed20810d82669a
saturnfugu <achmdfzan@upi.edu>
2023-09-22 13:43:57 +0700

    prev repo copy

---
+++ Assets/Prototype1Script/AIController.cs
@@ -0,0 +1,171 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Rendering;
+
+public class AIController : MonoBehaviour
+{
+    public Transform player;
+    public NavMeshAgent agent;
+    // public float sightRange;
+    // private bool playerInSight = false;
+    public LayerMask whatIsPlayer;
+    public PlayerController playerController;
+    public Vector3 range;
+    public Transform[] waypoints;
+    private Animator anim;
+
+    [SerializeField] private EnemyState currentState;
+    [SerializeField] private float losingPlayerTimer = 0f;
+    [SerializeField] private AIVision aiVision;
+    private int currentWaypointIndex = 0;
+
+    enum EnemyState
+    {
+        non_active,
+        activating,
+        idle,
+        seeking,
+        chasing,
+        attacking
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        currentState = EnemyState.seeking;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if ()
+                playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+
+                if (playerInSightRange)
+                {
+                    agent.SetDestination(player.transform.position);
+                }
+                else
+                {
+                    agent.ResetPath();
+                }
+        // Direction from the enemy to the player
+        Vector3 directionToPlayer = player.position - transform.position;
+
+        // Check if there's a clear line of sight between the enemy and the player
+        RaycastHit hitInfo;
+        if (Physics.Raycast(transform.position, directionToPlayer, out hitInfo, sightRange, whatIsPlayer))
+        {
+            // The ray hit something on the player's layer, meaning the enemy can see the player
+            agent.SetDestination(player.transform.position);
+            // Debug.DrawRay(transform.position, directionToPlayer, Color.green);
+        }
+        else
+        {
+            // The ray did not hit the player's layer, meaning the player is not in sight
+            // Debug.DrawRay(transform.position, directionToPlayer, Color.red);
+            EnemyPatrol();
+        }
+        */
+        if (currentState == EnemyState.non_active)
+        {
+            // non_active logic
+        }
+        else if (currentState == EnemyState.activating)
+        {
+            // activating logic
+        }
+        else if (currentState == EnemyState.idle)
+        {
+            // idle logic
+        }
+        else if (currentState == EnemyState.seeking)
+        {
+            agent.stoppingDistance = 0f;
+
+            if (!agent.hasPath && waypoints.Length > 0)
+            {
+                MoveToWaypoint();
+            }
+
+            if (aiVision.GetCanSeePlayer())
+            {
+                currentState = EnemyState.chasing;
+            }
+        }
+        else if (currentState == EnemyState.chasing)
+        {
+            agent.stoppingDistance = 1.5f;
+            agent.SetDestination(player.position);
+
+            if (aiVision.GetLastAwareTimer() >= losingPlayerTimer)
+            {
+                currentState = EnemyState.seeking;
+            }
+
+            if (Vector3.Distance(transform.position, player.position) <= 1.5f)
+            {
+                StartCoroutine(Attack());
+            }
+        }
+        else if (currentState == EnemyState.attacking)
+        {
+            // attacking logic
+        }
+    }
+
+    private IEnumerator Attack()
+    {
+        Debug.Log(Vector3.Distance(transform.position, player.position));
+
+        currentState = EnemyState.attacking;
+        anim.SetTrigger("Attack");
+
+        yield return new WaitForSeconds(1f);
+
+        if (aiVision.GetCanSeePlayer())
+        {                
+            currentState = EnemyState.chasing;
+        }
+        else
+        {
+            currentState = EnemyState.seeking;
+        }
+    }
+
+    private void MoveToWaypoint()
+    {
+        int randomIndex;
+        do
+        {
+            randomIndex = Random.Range(0, waypoints.Length);
+        }
+        while (currentWaypointIndex == randomIndex);
+
+        currentWaypointIndex = randomIndex;
+        agent.SetDestination(waypoints[currentWaypointIndex].position);
+    }
+
+    /*
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            //give damage to player
+            playerController.TakeDamage();
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+       
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position,sightRange);
+    }
+    */
+}
---
+++ Assets/Prototype1Script/PlayerController.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 10.0f;
+    public float jumpHeight;
+    public Transform orientation;
+    public LayerMask whatIsGround;
+    public Transform groundCheck;
+    public float radCircle;
+    public bool isGrounded; 
+    
+    public int maxHealth = 3;
+    private Rigidbody player;
+    private float defaultSpeed;
+    private Vector3 defaultScale;
+    private Camera cam;
+    private float defaultFOV;
+    private bool isOn = true;
+    public float stamina = 100;
+    public bool canCrouch = true;
+    public bool canMove = true;
+
+    
+    private void Start()
+    {
+        player = GetComponent<Rigidbody>();
+        defaultSpeed = speed;
+        cam = Camera.main;
+        defaultFOV = cam.fieldOfView;
+        defaultScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float moveZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float moveX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        if (canMove)
+        {
+        transform.Translate(moveX,0,0);
+        transform.Translate(0,0,moveZ);
+        }
+        gameObject.transform.rotation = orientation.transform.rotation;
+        isGrounded = Physics.CheckSphere(groundCheck.position, radCircle, whatIsGround);
+
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            player.velocity = new Vector3(player.velocity.z,jumpHeight);
+
+            //transform.Translate(0, jumpHeight * Time.deltaTime, 0);
+            
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            //chage speed to 120% and change fov to close one
+            speed += (speed * 20/100);
+            Debug.Log(speed);
+            DepleteStamina(0.01f);
+            cam.fieldOfView = 60;
+
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            //chage speed to default and change fov to normal
+            cam.fieldOfView = defaultFOV;
+            speed = defaultSpeed;
+        }
+        if (canCrouch)
+        {
+            
+            if (Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                //chage speed to 50%
+                speed -= (speed * 50/100);
+                gameObject.layer = default;
+                transform.localScale = new Vector3(transform.localScale.x, 0.5f, transform.localScale.z); 
+                Debug.Log(speed);
+
+            }
+            else if (Input.GetKeyUp(KeyCode.LeftControl))
+            {
+                speed = defaultSpeed;
+                transform.localScale = defaultScale;
+                gameObject.layer = 3;
+            }
+        }
+
+        
+    }
+
+    private void DepleteStamina(float amount)
+    {
+        stamina -= amount;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(groundCheck.position, radCircle);
+
+    }
+    public void TakeDamage()
+    {
+        maxHealth -= 1;
+        if (maxHealth == 0)
+        {
+            Die();
+        }
+    }
+    private void Die()
+    {
+        gameObject.active = false;
+    }
+}

1df949e60021a3cc6bd4dd063ea1b17bd578bad4
Svmmervoid <farhanaulia64@gmail.com>
2023-10-10 23:31:56 +0700

    Update Flashlight Audio

--- Assets/Suara MC/Scripts/MovementAudio.cs
+++ Assets/Suara MC/Scripts/MovementAudio.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class footsteps : MonoBehaviour
+public class MovementAudio : MonoBehaviour
 {
     public AudioSource footstepsSound, sprintSound;
 

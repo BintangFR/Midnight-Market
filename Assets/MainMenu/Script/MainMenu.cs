dab927da62bd5ffe803c5e66194809bb3eb264a3
Siv3rn <anggito732@gmail.com>
2023-10-13 17:09:25 +0700

    Add enemy Asset to Build

--- Assets/MainMenu/Script/MainMenu.cs
+++ Assets/MainMenu/Script/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame ()
     {
-        SceneManager.LoadScene("Prototype-new");
+        SceneManager.LoadScene("MainGame");
     }
 
     public void QuitGame ()

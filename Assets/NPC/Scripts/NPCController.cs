e8bd348e8a33e78198bb1fdca2c1ed36ca0cacf5
Muhammad Rafli <mraflir96@gmail.com>
2023-12-04 20:59:52 +0700

    Update NPC Animation

--- Assets/NPC/Scripts/NPCController.cs
+++ Assets/NPC/Scripts/NPCController.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class NPCController : MonoBehaviour,IInteractable
+public class NPCController : MonoBehaviour, IInteractable
 {
-
     public GameObject npcDialogue;
-    private Animator npcAnimator; 
-
+    private Animator npcAnimator;
+    
     void Start()
     {
-        Transform childTransform = transform.Find("NPC Idle");
+        Transform childTransform = transform.Find("npc idle new");
 
         if (childTransform != null)
         {
@@ -25,19 +24,26 @@ public class NPCController : MonoBehaviour,IInteractable
     }
 
     public void Interact()
-    {        
-        npcDialogue.SetActive(true);
-
-        if (npcDialogue.activeSelf) 
+    {
+        if (!npcDialogue.activeSelf)
         {
-            if (npcAnimator != null)
+            npcDialogue.SetActive(true);
+
+            if (npcDialogue.activeSelf && npcAnimator != null)
             {
+                GameObject player = GameObject.FindWithTag("Player");
+                if (player != null)
+                {
+                    transform.LookAt(player.transform.position);
+                    transform.eulerAngles = new Vector3(-180f, transform.eulerAngles.y, -180f);
+                }
                 npcAnimator.SetTrigger("Talking");
             }
         }
     }
 
-    public void ChangeDialogue(Dialogue dialogue){
+    public void ChangeDialogue(Dialogue dialogue)
+    {
         npcDialogue = dialogue.gameObject;
     }
 }
